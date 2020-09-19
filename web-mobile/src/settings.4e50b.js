085362e9fbf759d9839ae89fae6f96cfba619bdf
yanwenhuang <yanwen.huang@igg.com>
2020-09-19 22:08:52 +0800

    部署web

---
+++ web-mobile/src/settings.4e50b.js
@@ -0,0 +1,24 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scene/denglu.fire",
+    orientation: "portrait",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "c9161",
+        resources: "8a11c",
+        main: "367e1"
+    }
+};
