83fbbe4f03b61af100ed933b933d779229b6db53
xuanyuanstate <xuanyuanstate@outlook.com>
2020-10-05 19:51:29 +0800

    测试

---
+++ web-mobile/src/settings.e3e75.js
@@ -0,0 +1,26 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scene/denglu.fire",
+    orientation: "portrait",
+    debug: true,
+    jsList: [
+        "assets/script/data/protobuf/protobuf.388c3.js"
+    ],
+    bundleVers: {
+        resources: "dfe37",
+        internal: "493b3",
+        main: "2f9de"
+    }
+};
