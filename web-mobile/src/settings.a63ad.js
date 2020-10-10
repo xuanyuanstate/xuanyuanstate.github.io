45a9325a46b95b0f75f248e07c2f9c3aeaf092ac
yanwenhuang <yanwen.huang@igg.com>
2020-10-11 00:04:16 +0800

    重新部署

--- web-mobile/src/settings.a63ad.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/denglu.fire",orientation:"portrait",jsList:["assets/script/data/protobuf/protobuf.67b96.js"],bundleVers:{resources:"e8ef6",internal:"f1413",main:"745b3"}};
