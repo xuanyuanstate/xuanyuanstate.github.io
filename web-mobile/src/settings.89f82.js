ed5000c4b96f5023141895b01320f69e6fce458e
yanwenhuang <yanwen.huang@igg.com>
2020-11-04 22:32:20 +0800

    重新部署

--- web-mobile/src/settings.89f82.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/denglu.fire",orientation:"portrait",jsList:["assets/script/data/protobuf/protobuf.67b96.js"],bundleVers:{internal:"f1413",resources:"5f12a",main:"673b3"}};
